2c29c5d000bb1535e553e89dc762671841f4ee3b
Dmytro Muzyka <47075277+Mich47@users.noreply.github.com>
2023-02-03 21:50:27 +0200

    refactoring to Redux

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,29 @@
-import { nanoid } from 'nanoid';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addContact, removeContact, setFilter } from 'redux/actions';
 import { ContactForm } from './Phonebook/ContactForm';
 import { ContactList } from './Phonebook/ContactList';
 import { Filter } from './Phonebook/Filter';
 import { Section } from './Phonebook/Section';
 
-const CONTACTS = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
-
 export const App = () => {
-  const [contacts, setContacts] = useState(() => {
-    const contactsInLS = localStorage.getItem('contacts');
-    if (contactsInLS) {
-      return JSON.parse(contactsInLS);
-    }
-    return CONTACTS;
-  });
+  const contacts = useSelector(state => state.contacts);
+  // const [contacts, setContacts] = useState(() => {
+  //   const contactsInLS = localStorage.getItem('contacts');
+  //   if (contactsInLS) {
+  //     return JSON.parse(contactsInLS);
+  //   }
+  //   return CONTACTS;
+  // });
 
-  const [filter, setFilter] = useState('');
+  const filter = useSelector(state => state.filter);
+
+  const dispatch = useDispatch();
 
   const handleChangeFilter = event => {
     const { value } = event.target;
-    setFilter(value);
+
+    dispatch(setFilter(value));
   };
 
   useEffect(() => {
@@ -34,10 +32,6 @@ export const App = () => {
 
   const handleSubmitForm = (event, name, number) => {
     event.preventDefault();
-    return handleAddedContact(name, number);
-  };
-
-  const handleAddedContact = (name, number) => {
     //Контакт вже існує, повертає false
     if (contacts.some(contact => contact.name === name)) {
       alert(`${name} is already in contacts.`);
@@ -45,12 +39,12 @@ export const App = () => {
     }
 
     //Інакше додає новий контакт, повертає true
-    setContacts(prev => [...prev, { id: nanoid(), name, number }]);
+    dispatch(addContact(name, number));
     return true;
   };
 
-  const handleDeleteContact = id =>
-    setContacts(prev => prev.filter(item => item.id !== id));
+  const handleDeleteContact = id => dispatch(removeContact(id));
+  // setContacts(prev => prev.filter(item => item.id !== id));
 
   const handleFilterContacts = query =>
     contacts.filter(({ name }) =>
